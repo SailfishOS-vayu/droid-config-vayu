e37c0f745fbd9773b5ace4e6149e5014cb505723
Jami Kettunen <jami.kettunen@protonmail.com>
2021-03-12 01:25:17 +0200

    profile.d: Adapt scripts to work with SFOS 4.0
    It seems we no longer have proper bash, but instead
    busybox-symlinks-bash which doesn't work with the wildcard syntax.

--- sparse/etc/profile.d/devel-su-sudo-alias.sh
+++ sparse/etc/profile.d/devel-su-sudo-alias.sh
@@ -1,4 +1,3 @@
 # Add convenience 'sudo' alias for 'devel-su' if sudo command isn't found
-if [[ $- = *i* && ! -x `command -v sudo` ]]; then
+command -v sudo > /dev/null || \
 	alias sudo="devel-su"
-fi
