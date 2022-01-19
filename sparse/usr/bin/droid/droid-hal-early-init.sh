a13172960e3224635889f773d9771f755e3dcd71
Verevka <verevka86@gmail.com>
2022-01-19 18:36:58 +0300

    create a metadata partition and mount

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -7,6 +7,9 @@ super_part=/dev/sda23
 modem_part=/dev/sde52
 dsp_part=/dev/sde48
 bluetooth_part=/dev/sde27
+
+# Partition Metadata
+metadata_part=/dev/sda19
  
 dmesg_info() {
     echo "[mount-partitions.sh] $@" > /dev/kmsg
@@ -18,7 +21,7 @@ dmsetup create --concise "$(/usr/bin/parse-android-dynparts $super_part)"
 dmesg_info "Dynamic partitions: $(ls /dev/mapper/dynpart-*)"
  
 dmesg_info "Mount dynamic partitions"
-mkdir -p /system_root /system_ext /vendor /odm2 /product /mnt
+mkdir -p /system_root /system_ext /vendor /odm2 /product /mnt /metadata
  
 dmesg_info "$(mount -v -o ro /dev/mapper/dynpart-system  /system_root)"
 dmesg_info "$(mount --bind /system_root/system /system)"
@@ -32,6 +35,9 @@ dmesg_info "Mount /vendor/*"
 dmesg_info "$(mount -v $modem_part      /vendor/firmware_mnt)"
 dmesg_info "$(mount -v $dsp_part        /vendor/dsp)"
 dmesg_info "$(mount -v $bluetooth_part  /vendor/bt_firmware)"
+
+dmesg_info "Mount metadata*"
+dmesg_info "$(mount -v $metadata_part   /metadata)"
  
 # comment out when everything works
 dmesg_info "$(findmnt)"
