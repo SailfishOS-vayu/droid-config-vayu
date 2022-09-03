662530013563d4b27bfcf19cc246be6faa0443d3
Verevka <verevka86@gmail.com>
2022-09-03 15:15:51 +0300

    Mount custom system,vendor images handled by systemd

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,47 +1,25 @@
 #!/bin/bash
-# Verbose script for mounting custom partitions
- 
+
 # Custom Partitions mounted
 modem_part=/data/.stowaways/firmware/modem.img
 dsp_part=/data/.stowaways/firmware/dsp.img
 bluetooth_part=/data/.stowaways/firmware/bluetooth.img
-vendor_part=/data/.stowaways/firmware/vendor.img
-system_part=/data/.stowaways/firmware/system.img
-system_ext_part=/data/.stowaways/firmware/system_ext.img
-product_part=/data/.stowaways/firmware/product.img
 metadata_part=/data/.stowaways/firmware/metadata.img
 
 # custom patches
 lipstick_patch=/data/.stowaways/patches/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
 
-dmesg_info() {
-    echo "[mount-partitions.sh] $@" > /dev/kmsg
-}
-
-dmesg_info "Mount dynamic partitions"
-mkdir -p /system_root /system_ext /vendor /product /mnt /metadata
-
 # Android binder links
-dmesg_info "$(ln -svf /dev/binderfs/anbox-binder /dev/anbox-binder)"
-dmesg_info "$(ln -svf /dev/binderfs/anbox-hwbinder /dev/anbox-hwbinder)"
-dmesg_info "$(ln -svf /dev/binderfs/anbox-vndbinder /dev/anbox-vndbinder)"
-dmesg_info "$(ln -svf /dev/binderfs/puddlejumper /dev/puddlejumper)"
-dmesg_info "$(ln -svf /dev/binderfs/hwpuddlejumper /dev/hwpuddlejumper)"
-dmesg_info "$(ln -svf /dev/binderfs/vndpuddlejumper /dev/vndpuddlejumper)"
+ln -svf /dev/binderfs/puddlejumper /dev/puddlejumper
+ln -svf /dev/binderfs/hwpuddlejumper /dev/hwpuddlejumper
+ln -svf /dev/binderfs/vndpuddlejumper /dev/vndpuddlejumper
 
-
-dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $system_part /system_root)"
-dmesg_info "$(mount --bind /system_root/system /system)"
-dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $system_ext_part /system_ext)"
-dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $vendor_part /vendor)"
-dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $product_part /product)"
-dmesg_info "$(mount -v -o loop,ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337 -t vfat $modem_part  /vendor/firmware_mnt)"
-dmesg_info "$(mount -v -o loop,ro,nosuid,nodev,barrier=1 -t ext4 $dsp_part /vendor/dsp)"
-dmesg_info "$(mount -v -o loop,ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 -t vfat $bluetooth_part /vendor/bt_firmware)"
-dmesg_info "$(mount -v -o loop,ro,noatime,nosuid,nodev,discard -t ext4 $metadata_part /metadata)"
+mount -v -o loop,ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337 -t vfat $modem_part  /vendor/firmware_mnt
+mount -v -o loop,ro,nosuid,nodev,barrier=1 -t ext4 $dsp_part /vendor/dsp
+mount -v -o loop,ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 -t vfat $bluetooth_part /vendor/bt_firmware
+mount -v -o loop,ro,noatime,nosuid,nodev,discard -t ext4 $metadata_part /metadata
 
 # bind custom patches
-dmesg_info "$(mount --bind $lipstick_patch /usr/share/lipstick-jolla-home-qt5/statusarea/StatusArea.qml)"
+mount --bind $lipstick_patch /usr/share/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
 
-# comment out when everything works
-dmesg_info "$(findmnt)"
+echo -1000 > /proc/self/oom_score_adj
