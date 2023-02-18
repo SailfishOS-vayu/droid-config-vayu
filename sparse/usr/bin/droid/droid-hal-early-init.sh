d6c853a4960f91ce2aed81d884f8cb035c49393b
Verevka <verevka86@gmail.com>
2023-02-18 12:48:18 +0300

    temp: disable clock patch

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -7,7 +7,7 @@ bluetooth_part=/data/.stowaways/firmware/bluetooth.img
 metadata_part=/data/.stowaways/firmware/metadata.img
 
 # custom patches
-lipstick_patch=/data/.stowaways/patches/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
+#lipstick_patch=/data/.stowaways/patches/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
 
 mount -v -o loop,ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337 -t vfat $modem_part  /vendor/firmware_mnt
 mount -v -o loop,ro,nosuid,nodev,barrier=1 -t ext4 $dsp_part /vendor/dsp
@@ -15,6 +15,6 @@ mount -v -o loop,ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 -t vfa
 mount -v -o loop,ro,noatime,nosuid,nodev,discard -t ext4 $metadata_part /metadata
 
 # bind custom patches
-mount --bind $lipstick_patch /usr/share/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
+#mount --bind $lipstick_patch /usr/share/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
 
 echo -1000 > /proc/self/oom_score_adj
